0924320157f600785441c08da2f9b00f68f72033
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-05-23 17:30:02 +0000

    Fix: Clean nextjs app template

--- assets/src/app/page.tsx
+++ assets/src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main>
-      <h1>Bienvenido/a a nuestra applicacion</h1>
-      <p>Hola, estamos felices de verte aqui. Por favor, regrese mas tarde para ver los mejores contenidos del mundo.</p>
+      <h1>Welcome to my awesome page that almost took my sanity away for a week.</h1>
     </main>
   )
 }
