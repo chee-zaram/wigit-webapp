e0201514fc9d0e88631366c83033aef8484a8ba7
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-05-23 03:04:59 +0000

    Feat: Setup Nextjs

---
+++ assets/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Bienvenido/a a nuestra applicacion</h1>
+      <p>Hola, estamos felices de verte aqui. Por favor, regrese mas tarde para ver los mejores contenidos del mundo.</p>
+    </main>
+  )
+}
