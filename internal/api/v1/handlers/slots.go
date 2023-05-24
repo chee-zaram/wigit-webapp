22efee240aa56a614d9a2170227b4f8c54344a09
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-05-24 14:14:20 +0100

    feat(handlers): define handler for post request to `/slots` endpoint

--- internal/api/v1/handlers/slots.go
+++ internal/api/v1/handlers/slots.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
 	"time"
 
@@ -25,4 +26,59 @@ func GetSlots(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{"data": slots})
+}
+
+// AdminPostSlots adds a new slot to the database.
+func AdminPostSlots(ctx *gin.Context) {
+	_slot := new(models.Slot)
+
+	if err := ctx.ShouldBindJSON(_slot); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := validatePostSlotsData(_slot); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := DBConnector.Query(func(tx *gorm.DB) error {
+		return tx.Create(_slot).Error
+	}); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": ErrInternalServer.Error()})
+		return
+	}
+
+	slot, err := getSlotFromDB(*_slot.ID)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"msg":  "Slot created successfully",
+		"data": slot,
+	})
+}
+
+// validatePostSlotsData checks the validity of the json fields provided during
+// a post request by an admin.
+func validatePostSlotsData(slot *models.Slot) error {
+	if slot.DateTime.Compare(time.Now()) < 1 {
+		return errors.New("Date and time for slot must be in the future")
+	}
+	return nil
+}
+
+// getSlotFromDB retrieves a slot with id from the database.
+func getSlotFromDB(id string) (*models.Slot, error) {
+	slot := new(models.Slot)
+
+	if err := DBConnector.Query(func(tx *gorm.DB) error {
+		return tx.First(slot, "id = ?", id).Error
+	}); err != nil {
+		return nil, ErrInternalServer
+	}
+
+	return slot, nil
 }
