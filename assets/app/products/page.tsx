44760f45e7df8c4246f69a0256d9991e85de8a2b
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-05-25 01:41:21 +0000

    Feat: Add more components

--- assets/app/products/page.tsx
+++ assets/app/products/page.tsx
@@ -1,16 +1,24 @@
 // The products page for the wigit web app
+"use client";
+
+import useFetch from "@app/hooks/useFetch";
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal, PromiseLikeOfReactNode } from "react";
 
 const url: string = "https://jsonplaceholder.typicode.com/todos"
 
-export const metadata = { title: 'wigit products' };
+// export const metadata = { title: 'wigit products' };
 
 export default async function Products() {
-  const res: Response = await fetch(url);
-  const data = await res.json();
+  // const res: Response = await fetch(url);
+  // const data = await res.json();
+  const data = useFetch(url);
   return (
     <div>
       <h1>Our wigs</h1>
-        <p>Nothing but class.... { data[1].title }</p>
+        <p>Nothing but class....</p>
+        { data && data.map((item: any) => {
+          <p key={ item.id }>{ item.title }</p>
+        }) }
     </div>
   )
 }
