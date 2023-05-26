f17f83941ad5ffeb22eb70a7c5a8ffc0bfd40cfa
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-05-26 09:45:33 +0100

    feat(handlers): configure handler to update user information

--- internal/api/v1/handlers/users.go
+++ internal/api/v1/handlers/users.go
@@ -11,14 +11,14 @@ import (
 
 // CustomerDeleteUser deletes the current user from the database.
 func CustomerDeleteUser(ctx *gin.Context) {
-	_, id, err := validateUserParams(ctx)
+	user, _, err := validateUserParams(ctx)
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	if err := DBConnector.Query(func(tx *gorm.DB) error {
-		return tx.Exec(`DELETE FROM users WHERE id = ?`, id).Error
+		return tx.Exec(`DELETE FROM users WHERE id = ?`, *user.ID).Error
 	}); err != nil {
 		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -31,31 +31,73 @@ func CustomerDeleteUser(ctx *gin.Context) {
 
 // CustomerPutUser updates a user's information in the database.
 func CustomerPutUser(ctx *gin.Context) {
-	_, _, err := validateUserParams(ctx)
+	user, newUser, err := validateUserParams(ctx)
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	ctx.JSON(http.StatusAccepted, gin.H{"msg": "Handler not implemented yet"})
+	updateUserInfo(user, newUser)
+	if err := DBConnector.Query(func(tx *gorm.DB) error {
+		return tx.Save(user).Error
+	}); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedUser, err := getUserByID(*user.ID)
+	if err != nil {
+		updatedUser = user
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"msg":  "User updated successfully",
+		"data": updatedUser,
+	})
 }
 
 // validateUserParams validates data sent to the `users` endpoint.
 // It is used during updating and deletion of a user or information.
-func validateUserParams(ctx *gin.Context) (*models.User, string, error) {
+func validateUserParams(ctx *gin.Context) (*models.User, *models.User, error) {
 	_user, exists := ctx.Get("user")
 	if !exists {
-		return nil, "", errors.New("User not set in context")
+		return nil, nil, errors.New("User not set in context")
 	}
 	user := _user.(*models.User)
 	id := ctx.Param("user_id")
 	if id == "" {
-		return nil, "", errors.New("User ID must be set")
+		return nil, nil, errors.New("User ID must be set")
 	}
 
 	if id != *user.ID {
-		return nil, "", errors.New("Cannot perform operation on another user's account")
+		return nil, nil, errors.New("Cannot perform operation on another user's account")
 	}
 
-	return user, id, nil
+	newUser := new(models.User)
+	if err := ctx.ShouldBind(newUser); err != nil {
+		return nil, nil, err
+	}
+
+	return user, newUser, nil
+}
+
+// updateUserInfo updates a user's information on put request.
+func updateUserInfo(user, newUser *models.User) {
+	user.Email = newUser.Email
+
+	if newUser.FirstName != nil && *newUser.FirstName != "" {
+		user.FirstName = newUser.FirstName
+	}
+
+	if newUser.LastName != nil && *newUser.LastName != "" {
+		user.LastName = newUser.LastName
+	}
+
+	if newUser.Address != nil && *newUser.Address != "" {
+		user.Address = newUser.Address
+	}
+
+	if newUser.Phone != nil && *newUser.Phone != "" {
+		user.Phone = newUser.Phone
+	}
 }
