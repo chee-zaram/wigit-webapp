4b8681f8d5bf9e838f5dc3710dc87b833c641c35
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-05-27 05:05:51 +0000

    Feat: Implement sign in

--- assets/app/signin/page.tsx
+++ assets/app/signin/page.tsx
@@ -2,10 +2,20 @@
 "use client";
 import { useState } from 'react';
 import SignInForm from '@app/signin/components/SignInForm';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 const signin = () => {
     // check if user is signed in
     const [ isSignedIn, setIsSignedIn ] = useState(false);
+    
+    const { data, error } = useQuery({ queryKey: ['signInSubmit'], queryFn: handleAxios})
+    console.log(data);
+    async function handleAxios () {
+        const { data } = await axios.post("https://jel1cg-8000.csb.app/signin", { headers: {"Authorization": "newBossVee", "Content-Type": "Application/json"}, token: 'something sent from the client side'});
+        console.log(data ? data : error);
+        setIsSignedIn(true);
+    }
 
     return (
         <main className='signin_main flex flex-col justify-around items-center'>
