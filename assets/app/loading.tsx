7e0185d111fec1a84f07cee2a460f7ff345921e7
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-05-27 00:28:47 +0000

    Feat: Add home loading page

---
+++ assets/app/loading.tsx
@@ -0,0 +1,11 @@
+// home page loading page
+// design loading skeleton
+
+const homeLoading = () => (
+    <div>
+        <h3>Give us a moment</h3>
+        <p>You'll see our beautiful products soon...</p>
+    </div>
+);
+
+export default homeLoading;
