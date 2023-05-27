011f297fc2d3325818d397622ff40719a9a1f25f
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-05-27 07:50:56 +0000

    Fix: Edit middleware return statement bug

--- assets/middleware.tsx
+++ assets/middleware.tsx
@@ -1,11 +1,11 @@
 // middleware for restricted routes
 import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export async function Middleware (request: NextRequest) {
+export function Middleware(request: NextRequest) {
     // pass for now
     if (request.nextUrl.pathname.startsWith('signin')) {
-        return NextResponse.rewrite(new URL('mad-loggin', request.url)
+        return NextResponse.rewrite(new URL('/mad-login', request.url));
     }
     return NextResponse.next;
 }
