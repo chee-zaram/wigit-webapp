0158f4bc0e1cb7cfb1b2dca073eed7f15830cfe2
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-06-05 16:38:33 +0000

    Feat: handle checkout

--- assets/app/cart/components/ShoppingCart.tsx
+++ assets/app/cart/components/ShoppingCart.tsx
@@ -1,4 +1,4 @@
-// shopping cart homepage
+// shopping cart component
 "use client";
 
 import { useState, useEffect } from 'react';
@@ -10,12 +10,14 @@ import { NextPage } from 'next';
 const ShoppingCart: NextPage<Item> = async (props) => {
 
     return (
-        <main className='md:d:container'>
+        <main className='md:container'>
             <div className='flex center gap-4 p-8 border border-color-slate-700'>
                 <h1>product name</h1>
                 <h2>amount = { props.amount }</h2>
+                <h2>qty = { props.quantity }</h2>
+
                 {/* <p>{ productObj. name }</p> */}
-                <button className='border p-4 rounded-full bg-blue-500'0'>-</button>
+                <button className='border p-4 rounded-full bg-blue-500'>-</button>
                 <button className='border p-4 rounded-full bg-blue-500'>+</button>
                 <button className='border p-4 rounded-full bg-blue-500'>remove</button>
             </div>
--- assets/app/cart/page.tsx
+++ assets/app/cart/page.tsx
@@ -5,18 +5,42 @@ import { useState, useEffect } from 'react';
 import { useSignInContext } from '@app/SignInContextProvider';
 import ShoppingCart from '@app/cart/components/ShoppingCart';
 import axios from 'axios';
+import Button from '@components/Button';
 
 const url = 'https://cheezaram.tech/api/v1/cart';
 
-
-
 const Cart = () => {
+    const [ deliveryMethod, setDeliveryMethod ] = useState('');
     const { jwt } = useSignInContext();
     const [ cart, setCart ] = useState<any> ([]);
+    const headers = {'Authorization': 'Bearer ' + jwt};
     const getCart = () => {
-    fetch(url, {headers: {'Authorization': 'Bearer ' + jwt}})
+    fetch(url, {headers: headers})
     .then(res => res.json())
     .then(data => setCart(data.data))
+    };
+    
+    // const handleSetDelivery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     console.log(deliveryMethod);
+    //     event.preventDefault();
+    //     setDeliveryMethod(event.target.value);
+    //     console.log(deliveryMethod);
+    // };
+    const handlePickup = () => {
+        setDeliveryMethod('pickup');
+        console.log(deliveryMethod);
+    }
+    const handleDelivery = () => {
+        setDeliveryMethod('delivery');
+        console.log(deliveryMethod);
+    }
+    
+    const handleSubmit = async() => {
+        const cartData = {cart: cart, delivery_method: deliveryMethod}
+        const { data, status } = await axios.post(url, headers, cartData);
+        // if (status === 'ok') {
+        
+        // }
     };
     
     useEffect(getCart, []);
@@ -34,7 +58,17 @@ const Cart = () => {
             </div> :
             <p>no items in cart</p>
             }
-            
+            <form onSubmit={ handleSubmit }>
+                <div>
+                    <input required onClick={ handlePickup } id='pickup' name='delivery_method' type='radio' value='pickup' />
+                    <label htmlFor='pickup'>pickup</label>
+                </div>
+                <div>
+                    <input required onClick={ handleDelivery } id='delivery' name='delivery_method' type='radio' value='delivery' />
+                    <label htmlFor='delivery'>delivery</label>
+                </div>
+                <Button text='checkout' type='submit' />
+            </form>
         </main>
     )
 };
