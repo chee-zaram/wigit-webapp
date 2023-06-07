a0f576a31f45b4dbb5c3314c68b0ab76781e29ce
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-06-07 12:47:13 +0100

    revert "ci: move script commands in deploy job into deploy script"
    This reverts commit 737aee34817f1abb13c2d43b001e0351e2c3a05c.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,5 @@
 
 # Exit if any command returns an error code
 set -e
-cd webapp
-git stash && git checkout main && git pull origin
 GOOS=linux GOARCH=amd64 /usr/local/go/bin/go build -o "$BE_EXEC"
 sudo service "$BE_EXEC" restart
