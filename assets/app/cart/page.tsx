62644ce3772ac9880071641de8938889cd8017ae
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-06-12 21:52:21 +0000

    Fix: handle checkout error

--- assets/app/cart/page.tsx
+++ assets/app/cart/page.tsx
@@ -50,17 +50,22 @@ if (typeof window !== 'undefined') {
         setDeliveryMethod('delivery');
         console.log(deliveryMethod);
     }
-    
     const handleSubmit = async(event: any) => {
         event.preventDefault();
 
         const cartData = { delivery_method: deliveryMethod };
-        const { data, status } = await axios.post(orderUrl, cartData, {headers: headers});
-        router.push('/');
-        alert('order sent');
-        // if (status === 'ok') {
-        
-        // }
+        try {
+            const { status } = await axios.post(orderUrl, cartData, {headers: headers});
+            
+            if ( status == 200 ) {
+                router.push('/');
+                alert('order sent'); 
+            }
+        }
+        catch(error) {
+            //catch it here
+            alert(`${error}`);
+        }
     };
     const handleEmptyCart = async() => {
         await axios.delete(url, {headers: headers});
@@ -74,34 +79,34 @@ if (typeof window !== 'undefined') {
         <main>
             
             { jwt !== 'not authorized' ?
-            <div>
-                <button onClick={handleEmptyCart}>empty cart</button>
-                <h2>shopping cart</h2>
-                <p>Sha pay and checkout</p>
-                <p>{role}</p>
-                { cart && cart.map((item: Item) => (
-                <div key={item.id}>
-                    <ShoppingCart { ...item } />
-                </div>
-                )) }
-                <form onSubmit={ handleSubmit }>
-                <h2> you chose {deliveryMethod}</h2>
-                <div>
-                    <input required onClick={ handlePickup } id='pickup' name='delivery_method' type='radio' value='pickup' />
-                    <label htmlFor='pickup'>pickup</label>
-                </div>
-                <div>
-                    <input required onClick={ handleDelivery } id='delivery' name='delivery_method' type='radio' value='delivery' />
-                    <label htmlFor='delivery'>delivery</label>
-                </div>
-                <Button text='checkout' type='submit' />
-            </form>
-            </div> :
-            <div className='cart_signin mx-auto w-[80vw] h-[40vh]'>
-                <p className='bg-light_bg/70 p-8 rounded' >Please <button className='text-accent underline hover:text-accent/60' onClick={ () => router.push('/signin')}>sign in</button> to shop with us</p>
+        <div>
+            <h2>shopping cart</h2>
+            <button onClick={handleEmptyCart}>empty cart</button>
+            <p>Sha pay and checkout</p>
+            <p>{role}</p>
+            { cart && cart.map((item: Item) => (
+            <div key={item.id}>
+                <ShoppingCart { ...item } />
             </div>
-            }
-        </main>
+            )) }
+            <form onSubmit={ handleSubmit }>
+            <h2> you chose {deliveryMethod}</h2>
+            <div>
+                <input required onClick={ handlePickup } id='pickup' name='delivery_method' type='radio' value='pickup' />
+                <label htmlFor='pickup'>pickup</label>
+            </div>
+            <div>
+                <input required onClick={ handleDelivery } id='delivery' name='delivery_method' type='radio' value='delivery' />
+                <label htmlFor='delivery'>delivery</label>
+            </div>
+            <Button text='checkout' type='submit' />
+        </form>
+        </div> :
+        <div className='cart_signin mx-auto w-[80vw] h-[40vh]'>
+            <p className='bg-light_bg/70 p-8 rounded' >Please <button className='text-accent underline hover:text-accent/60' onClick={ () => router.push('/signin')}>sign in</button> to shop with us</p>
+        </div>
+        }
+    </main>
     )
 };
 
