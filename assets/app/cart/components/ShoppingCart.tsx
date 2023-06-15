f53d684a5c4725f6f6273e2c8c0145dec6fce509
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-06-15 02:22:51 +0000

    Fix: Handle remove items bug

--- assets/app/cart/components/ShoppingCart.tsx
+++ assets/app/cart/components/ShoppingCart.tsx
@@ -12,6 +12,7 @@ import Image from 'next/image';
 const ShoppingCart: any = async (props: Item) => {
     const [ newQty, setNewQty ] = useState(props.quantity);
     const [ newAmount, setNewAmount ] = useState(Number(props.amount));
+    const [ hideItem, setHideItem ] = useState(false);
     const { jwt } = useSignInContext();
     const headers = {'Authorization': 'Bearer ' + jwt};
     const router = useRouter();
@@ -44,11 +45,11 @@ const ShoppingCart: any = async (props: Item) => {
     const handleRemoveItem = async() => {
         await axios.delete('https://cheezaram.tech/api/v1/cart/' + props.id, {headers: headers});
         // router.push('/');
-        
+        setHideItem(true);
     };
 
     return (
-        <section className='flex p-2 justify-between min-h-[150px] border-b border-t border-slate-700'>
+        <section className={hideItem ? 'hidden' : 'flex p-2 justify-between min-h-[150px] border-b border-t border-slate-700'}>
             <div className=' w-[80px] overflow-ellipsis'>
                 <div className='mx-auto w-[70px]'>
                     <Image src={ props.product.image_url } alt={ props.product.name } width={70} height={50}
