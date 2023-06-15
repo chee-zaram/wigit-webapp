8061255b2344f6e21868a65f20055a84f89d234e
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-06-15 02:51:04 +0000

    Fix: Remove link wrap from logo

--- assets/components/Logo.tsx
+++ assets/components/Logo.tsx
@@ -6,14 +6,12 @@ import logo from '/public/assets/images/logo.svg';
 
 const Logo = () => (
     <div className='logo_wrap bg-white h-8'>
-      <Link href='/'>
         <Image
           src={logo}
           alt='Wigit Company Logo'
           width={70}
           height={60}
         />
-      </Link>
     </div>
 );
 
