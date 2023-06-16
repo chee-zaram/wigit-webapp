f4f4c21062c535ac22b4a88924991952001621ff
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-06-16 09:31:45 +0000

    Fix: Add navigation to settings page

--- assets/app/profile/settings/page.tsx
+++ assets/app/profile/settings/page.tsx
@@ -65,6 +65,9 @@ const Settings = () => {
     
     return (
         <div>
+            <div onClick={() => {router.back()}} className='hover:bg-accent/80 text-right ml-[10vw] duration-300 rounded-full p-3 max-w-max'>
+               <svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40"><path d="M480-160 160-480l320-320 42 42-248 248h526v60H274l248 248-42 42Z"/></svg> 
+            </div>
             <h3 className='p-4 text-2xl font-bold mb-4 text-dark_bg/70'>Settings</h3>
             <div>
                 { showDelete &&
