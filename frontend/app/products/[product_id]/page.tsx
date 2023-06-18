8f434ac56cac070cb0ac56478f9c207fa5019384
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-06-18 20:50:30 +0000

    Feat: Design product details page

--- frontend/app/products/[product_id]/page.tsx
+++ frontend/app/products/[product_id]/page.tsx
@@ -32,11 +32,19 @@ const ProductDetails = async ({params}: {params: {product_id: string} }) => {
     
     return (
         <section>
-            <div>
-                {product && <h2>{ product.name }</h2>}
-                <div></div>
-            </div>
-            <ToastContainer/>
+            { product &&
+                <div>
+                    <div className='flexbox gap-3 max-w-[80vw] mx-auto'>
+                        <h2 className='font-bold text-xl text-dark_bg/80 '>{ product.name }</h2>
+                        <div className='border-3 p-3 border-accent bg-dark_bg'>
+                            <Image src={product.image_url} alt={product.name} height={400} width={150} />
+                        </div>
+                        <p>{ product.description }</p>
+                        <p className='text-accent font-bold'>{ product.price }</p>
+                    </div>
+                    <ToastContainer/>
+                </div>
+            }
         </section>
     );
 
