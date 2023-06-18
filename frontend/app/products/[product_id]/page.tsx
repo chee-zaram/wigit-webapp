35d162c37ce41af29c8e9fd7f5a833d5d5a2d3ce
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-06-18 23:01:14 +0000

    Fix: Format details page

--- frontend/app/products/[product_id]/page.tsx
+++ frontend/app/products/[product_id]/page.tsx
@@ -8,7 +8,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const ProductDetails = async ({params}: {params: {product_id: string} }) => {
     const url = 'https://cheezaram.tech/api/v1/products/' + params.product_id;
-    let product = {};
+    let product;
     try {
         product = await getProducts(url);
     } catch (error) {
@@ -30,12 +30,12 @@ const ProductDetails = async ({params}: {params: {product_id: string} }) => {
                 <div>
                     <BackButton />
                     <div className='flexbox gap-3 max-w-[80vw] mx-auto'>
-                        <h2 className='font-bold text-xl text-dark_bg/80 '>{ product.name }</h2>
+                        <h2 className='font-bold capitalize text-xl text-dark_bg/80 '>{ product.name }</h2>
                         <div className='border-3 p-3 border-accent bg-dark_bg'>
-                            <Image src={product.image_url} alt={product.name} height={400} width={150} />
+                            <Image src={product.image_url} alt={product.name} height={400} width={250} />
                         </div>
                         <p>{ product.description }</p>
-                        <p className='text-accent font-bold'>{ product.price }</p>
+                        <p className='text-accent font-bold'>GHS { product.price }</p>
                     </div>
                     <ToastContainer/>
                 </div>
