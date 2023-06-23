b5bfb5589a0fe0182f0518ff6f56fc27113b61ba
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-06-23 05:15:33 +0000

    Fix: Edit link class

--- frontend/components/Socials.tsx
+++ frontend/components/Socials.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 const Socials = () => (
     <aside className='flex md:flex-col gap-1'>
         <a href='' target='_blank'><i className='fab fa-github hover:text-light_bg duration-500 text-sm'></i></a>
-        <a href='' target='_blank'><i class="fab fa-instagram hover:text-light_bg duration-500 text-sm"></i></a>
-        <a href='' target='_blank'><i class="fab fa-twitter hover:text-light_bg duration-500 text-sm"></i></a>
-        <a href='' target='_blank'><i class="fas fa-phone hover:text-light_bg duration-500 text-sm"></i></a>
-        <a href='' target='_blank'><i class="fas fa-at hover:text-light_bg duration-500 text-sm"></i></a>
+        <a href='' target='_blank'><i className="fab fa-instagram hover:text-light_bg duration-500 text-sm"></i></a>
+        <a href='' target='_blank'><i className="fab fa-twitter hover:text-light_bg duration-500 text-sm"></i></a>
+        <a href='' target='_blank'><i className="fas fa-phone hover:text-light_bg duration-500 text-sm"></i></a>
+        <a href='' target='_blank'><i className="fas fa-at hover:text-light_bg duration-500 text-sm"></i></a>
     </aside>
 );
 
