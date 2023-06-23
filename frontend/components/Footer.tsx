e160a0a28996cad64192d35d3a2b0623108003ea
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-06-23 05:04:14 +0000

    Feat: Finish up footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -5,22 +5,23 @@ import Feedback from "@components/Feedback";
 import Socials from "@components/Socials";
 
 const Footer = () => (
-    <footer className='footer min-h-[40vh] mt-8 max-w-[100vw] text-light_bg/50 text-xs'>
-        <section className='flex flex-col md:flex-row justify-center md:justify-around flex-wrap gap-7 py-4 items-center '>
+    <footer className='px-8 footer min-h-[40vh] mt-8 max-w-[100vw] text-light_bg/50 text-xs'>
+        <section className='flex flex-col md:flex-row justify-center md:justify-between flex-wrap gap-6 py-4 items-center'>
             <Feedback />
             <UsefulLinks />
             <Socials />
         </section>
         <section className='border-t py-2 border-light_bg/30'>
-            <h3 className='font-bold'>Developers</h3>
-            <span className='mr-4 pt-2'>
-                <a>github</a>
-                <a href={'mailto:evbodiovo@gmail.com'}>Ovy Evbodi</a>
+            <h3 className='font-bold mb-1'>Developers</h3>
+            <span className='mr-4'>
+                <a href='https://github.com/OvyEvbodi' target='_blank'><i className='fab fa-github hover:text-light_bg duration-500'></i></a>
+                <a href={'mailto:evbodiovo@gmail.com'} className='ml-2 hover:text-light_bg duration-500'>Ovy Evbodi</a>
             </span>
             <span>
-                <a>github</a>
-                <a href={'mailto:ecokeke21@gmail.com'}>Chee-zaram Okeke</a>
+                <a href='https://github.com/chee-zaram' target='_blank'><i className='fab fa-github hover:text-light_bg duration-500'></i></a>
+                <a href={'mailto:ecokeke21@gmail.com'} className='ml-2 hover:text-light_bg duration-500'>Chee-zaram Okeke</a>
             </span>
+            <p className='text-light_bg/10 pt-2'>&copy; Wigit 2023, all rights reserved.</p>
         </section>
     </footer>
 );
--- frontend/components/Socials.tsx
+++ frontend/components/Socials.tsx
@@ -2,10 +2,12 @@
 import Link from 'next/link';
 
 const Socials = () => (
-    <aside>
-        <p>instagram</p>
-        <p>phone</p>
-        <p>mail</p>
+    <aside className='flex md:flex-col gap-1'>
+        <a href='' target='_blank'><i className='fab fa-github hover:text-light_bg duration-500 text-sm'></i></a>
+        <a href='' target='_blank'><i class="fab fa-instagram hover:text-light_bg duration-500 text-sm"></i></a>
+        <a href='' target='_blank'><i class="fab fa-twitter hover:text-light_bg duration-500 text-sm"></i></a>
+        <a href='' target='_blank'><i class="fas fa-phone hover:text-light_bg duration-500 text-sm"></i></a>
+        <a href='' target='_blank'><i class="fas fa-at hover:text-light_bg duration-500 text-sm"></i></a>
     </aside>
 );
 
