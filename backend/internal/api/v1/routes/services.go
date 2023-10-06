c5e2ad02f3a82547ad4c2f001b82f8216b9af8c8
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-10-06 21:24:08 +0100

    backend: add support for caching on `services` route

--- backend/internal/api/v1/routes/services.go
+++ backend/internal/api/v1/routes/services.go
@@ -3,13 +3,14 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/wigit-ng/webapp/backend/internal/api/v1/handlers"
+	"github.com/wigit-ng/webapp/backend/internal/api/v1/middlewares"
 )
 
 // ServicesRoutes adds public routes to the services endpoint.
 func ServicesRoutes(api *gin.RouterGroup) {
-	api.GET("/services", handlers.GetServices)
-	api.GET("/services/:service_id", handlers.GetServiceByID)
-	api.GET("/services/trending", handlers.GetTrendingServices)
+	api.GET("/services", middlewares.Redis, handlers.GetServices)
+	api.GET("/services/:service_id", middlewares.Redis, handlers.GetServiceByID)
+	api.GET("/services/trending", middlewares.Redis, handlers.GetTrendingServices)
 }
 
 // AdminServicesRoutes add protected routes for the services endpoint.
